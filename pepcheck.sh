427858cb381d30b905a6670033ca8dffa5e298d6
vic <vic@>
2014-01-22 19:13:41 -0500

    run first test to see that runserver works, with contacts

---
+++ pepcheck.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+VENV_DIR="./tutorial"
+PEP8='pep8'
+
+if [ $($PEP8 --version) != '0.6.1' ]; then
+    echo "WARNING: pep8 version not '0.6.1.'"
+fi
+
+TEST_RESULT=0
+PEP8_RESULT=0
+TMP_FILE=$(tempfile)
+find . -iname "*.py" | \
+grep -v $VENV_DIR | \
+while read ln; do
+    pep8 --filename="*.py" --show-pep8  --show-source "$ln" | \
+        grep . >> $TMP_FILE
+done
+grep . $TMP_FILE
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+    PEP8_RESULT=1
+fi
+rm $TMP_FILE
+
+#cd .
+#python manage.py test
+#TEST_RESULT=$?
+
+exit $(( $TEST_RESULT + $PEP8_RESULT ))
