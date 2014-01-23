95b58ca277169d1f45b1ba01d55e34c1cd7defbf
Victory <git@dfhu.org>
2014-01-22 20:09:47 -0500

    test that admin page is up

--- ft/tests.py
+++ ft/tests.py
@@ -1,9 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000/')
+from django.test import LiveServerTestCase
 
-body = browser.find_element_by_tag_name('body')
-assert 'Django' in body.text
 
-browser.quit()
+class AdminTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_admin_site(self):
+
+        self.browser.get(self.live_server_url + '/admin/')
+
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
