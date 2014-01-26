6eb85144804139255e0d19a5b300a02f40ff6641
Victory <git@dfhu.org>
2014-01-26 18:28:14 -0500

    add validators for alpha and number

---
+++ contacts/user_contacts/tests/test_validator.py
@@ -0,0 +1,18 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from user_contacts.validators import (
+    validate_number,
+    validate_string)
+
+
+class ValidatorTest(TestCase):
+    def test_string_is_invalid_if_contains_numbers_or_special_chars(self):
+        with self.assertRaises(ValidationError):
+            validate_string('@test')
+            validate_string('tester#')
+
+    def test_number_is_invalid_if_contains_non_digit(self):
+        with self.assertRaises(ValidationError):
+            validate_number('1234ABC')
+            validate_number('1234#')
---
+++ contacts/user_contacts/validators.py
@@ -0,0 +1,14 @@
+import re
+
+from django.core.exceptions import ValidationError
+
+
+def validate_string(string):
+    if re.search('^[A-Za-z]+$', string) is None:
+        raise ValidationError(
+            'Invalid: something other than letters or is empty')
+
+def validate_number(value):
+    if re.search('^[0-9]+$', value) is None:
+        raise ValidationError(
+            'Invalid: something other than letters or is empty')
