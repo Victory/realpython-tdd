be72c24e2d24d18e1f8e09dd002771bb66f3d1c1
vic <vic@>
2014-01-26 15:21:45 -0500

    add a new contact view

--- contacts/user_contacts/views.py
+++ contacts/user_contacts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import (
     render,
     render_to_response)
+from django.template import RequestContext
+
 from user_contacts.models import (
     Phone,
     Person)
+from user_contacts.new_contact_form import ContactForm
 
 
 def home(request):
@@ -13,3 +16,12 @@ def home(request):
 def all_contacts(request):
     contacts = Phone.objects.all()
     return render_to_response('all.html', {'contacts': contacts})
+
+
+def add_contact(request):
+    person_form = ContactForm()
+    return render(
+        request,
+        'add.html',
+        {'person_form': person_form},
+        context_instance=RequestContext(request))
