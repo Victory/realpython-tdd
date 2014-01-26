d17907021fcef7cb2e3d1bc6e38699eb420f71c0
Victory <git@dfhu.org>
2014-01-26 14:27:36 -0500

    pep8

--- contacts/user_contacts/views.py
+++ contacts/user_contacts/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import (
     render,
     render_to_response)
 
+
 def home(request):
     return render_to_response('home.html')
+
 
 def all_contacts(request):
     contacts = Phone.objects.all()
