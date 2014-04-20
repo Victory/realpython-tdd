7a9c5b71ac40248f09895cfb432b7a52abaab01b
vic <git@dfhu.org>
2014-04-20 19:16:32 -0400

    update path of manage.py

--- run-tests.py
+++ run-tests.py
@@ -3,5 +3,5 @@
 from subprocess import call
 
 if __name__ == '__main__':
-    errs = call(["python", "manage.py", "test", "ft"])
+    errs = call(["python", "contacts/manage.py", "test", "ft"])
     exit(errs)
