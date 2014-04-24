a64aa7429ed04401cb1fc50d24f65f22f3715ad7
vic <git@dfhu.org>
2014-04-24 18:51:00 -0400

    using pyvirtualdisplay for xvfb

--- run-tests.py
+++ run-tests.py
@@ -2,8 +2,22 @@
 
 from subprocess import call
 
+from pyvirtualdisplay import Display
+
+
 if __name__ == '__main__':
-    errs = call(["python", "contacts/manage.py", "test", "ft.tests"])
-    errs = call(["python", "contacts/manage.py", "test", "ft.livevalidation"])
-    errs += call(["python", "contacts/manage.py", "test", "user_contacts"])
+    display = Display(visible=0, size=(1440, 900))
+    display.start()
+
+    errs = 0
+    tests = ['ft.tests', 'ft.livevalidation', "user_contacts"]
+    for test in tests:
+        print "running '%s'" % test
+        err = call(["python", "contacts/manage.py", "test", test])
+        if err:
+            print "Failed\n"
+        else:
+            print "Passed\n"
+        errs += err
+
     exit(errs)
