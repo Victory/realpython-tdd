e8aa27fdd440474789bdc5d450b7fa8213d27aaf
vic <git@dfhu.org>
2014-04-29 18:18:40 -0400

    finish up coverage

---
+++ contacts/user_contacts/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from user_contacts.models import (
+    Person,
+    Phone)
+
+class PersonTest(TestCase):
+    def test_unicode(self):
+        person = Person()
+        first_name = "fred"
+        last_name = "smith"
+        person.first_name = first_name
+        person.last_name = last_name
+        expected = "%s, %s" % (last_name, first_name)
+        actual = person.__unicode__()
+        self.assertEquals(expected, actual)
+
+
+class PhoneTest(TestCase):
+
+    def test_unicode(self):
+        phone = Phone()
+        number = "8675309"
+        phone.number = number
+
+        expected = number
+        actual = phone.__unicode__()
+        self.assertEquals(expected, actual)
--- coverage.sh
+++ coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 coverage run \
-    --omit="contacts/ft/*,contacts/user_contacts/tests/*" \
+    --omit="contacts/ft/*,contacts/user_contacts/tests/*,contacts/contacts/wsgi.py" \
     --source='contacts' \
     contacts/manage.py test user_contacts
 coverage report
