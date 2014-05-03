b61a3d52b14556d7ca2a26bb15819394de349014
vic <git@dfhu.org>
2014-05-03 18:44:34 -0400

    hello sauce - push an example test to saucelabs

---
+++ contacts/ft/HelloSauce.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import unittest
+import sauceclient
+
+from selenium import webdriver
+#from django.test import LiveServerTestCase
+
+from sauceclient import SauceClient
+
+USERNAME = "Victory"
+ACCESS_KEY = os.environ.get('SAUCE_ACCESS_KEY')
+
+sauce = SauceClient(USERNAME, ACCESS_KEY)
+
+browsers = [{"platform": "Mac OS X 10.9",
+             "browserName": "chrome",
+             "version": ""},
+            {"platform": "Windows 8.1",
+             "browserName": "internet explorer",
+             "version": "11"}]
+
+
+def on_platforms(platforms):
+    def decorator(base_class):
+        module = sys.modules[base_class.__module__].__dict__
+        for i, platform in enumerate(platforms):
+            d = dict(base_class.__dict__)
+            d['desired_capabilities'] = platform
+            name = "%s_%s" % (base_class.__name__, i + 1)
+            module[name] = type(name, (base_class,), d)
+    return decorator
+
+
+@on_platforms(browsers)
+class SaucheTest(unittest.TestCase):
+
+    def setUp(self):
+        self.desired_capabilities['name'] = self.id()
+
+        sauce_url = "http://%s:%s@ondemand.saucelabs.com:80/wd/hub"
+        self.driver = webdriver.Remote(
+            desired_capabilities=self.desired_capabilities,
+            command_executor=sauce_url % (USERNAME, ACCESS_KEY)
+        )
+        self.driver.implicitly_wait(30)
+
+    def tearDown(self):
+        print("Link to your job: "
+              "https://saucelabs.com/jobs/%s" % self.driver.session_id)
+        try:
+            if sys.exc_info() == (None, None, None):
+                sauce.jobs.update_job(self.driver.session_id, passed=True)
+            else:
+                sauce.jobs.update_job(self.driver.session_id, passed=False)
+        finally:
+            self.driver.quit()
+
+    def test_sauce(self):
+        self.driver.get('http://saucelabs.com/test/guinea-pig')
+        assert "I am a page title - Sauce Labs" in self.driver.title
--- run-tests.py
+++ run-tests.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 
 from subprocess import call
 
 from pyvirtualdisplay import Display
+
+try:
+    fh = open(os.path.dirname(os.path.realpath(__file__)) + '/sauce-key.txt')
+    key = fh.read().strip()
+    os.environ.setdefault('SAUCE_ACCESS_KEY', key)
+except:
+    if not os.environ.get('SAUCE_ACCESS_KEY'):
+        print "Warning Sauce Key Not Read"
 
 
 if __name__ == '__main__':
@@ -19,6 +28,17 @@ if __name__ == '__main__':
         display.start()
 
     errs = 0
+
+    saucetest = ['contacts/ft/HelloSauce.py']
+    for test in saucetest:
+        print "running '%s'" % test
+        err = call(["py.test", test])
+        if err:
+            print "Failed\n"
+        else:
+            print "Passed\n"
+        errs += err
+
     tests = ['ft.tests', 'ft.livevalidation', "user_contacts"]
     for test in tests:
         print "running '%s'" % test
