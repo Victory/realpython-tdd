de2afdc1ae740bb312a334d26f5d880f5b8e8c27
vic <git@dfhu.org>
2014-05-04 12:12:47 -0400

    use  self.driver.get(self.live_server_url)

--- contacts/ft/HelloDjangoSauce.py
+++ contacts/ft/HelloDjangoSauce.py
@@ -66,5 +66,5 @@ class SaucheTest(LiveServerTestCase):
             self.driver.quit()
 
     def test_sauce(self):
-        self.driver.get('http://localhost:8000/')
+        self.browser.get(self.live_server_url + "/")
         assert "Welcome." in self.driver.title
